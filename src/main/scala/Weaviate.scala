7b7c755a2b1092686da822099446c78c0e82919f
mrbean <sam@you.com>
2022-11-11 22:40:30 -0500

    start making some mapping code

--- src/main/scala/Weaviate.scala
+++ src/main/scala/Weaviate.scala
@@ -3,10 +3,11 @@ package io.weaviate.spark
 import org.apache.spark.sql.connector.catalog.{Table, TableProvider}
 import org.apache.spark.sql.connector.expressions.Transform
 import org.apache.spark.sql.sources.DataSourceRegister
-import org.apache.spark.sql.types.{DataTypes, Metadata, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{DataType, DataTypes, Metadata, StringType, StructField, StructType}
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 import technology.semi.weaviate.client.Config
 import technology.semi.weaviate.client.WeaviateClient
+import technology.semi.weaviate.client.v1.schema.model.Property
 
 import java.util
 import scala.jdk.CollectionConverters._
@@ -27,9 +28,19 @@ class Weaviate extends TableProvider with DataSourceRegister {
     if (result.getResult == null) throw new WeaviateClassNotFoundError("Class "+className+ " was not found.")
     val properties = result.getResult.getProperties.asScala
     // TODO p.getDataType returns List<string> so need to think about how to convert to Spark datatype
-    val structFields = properties.map(p => StructField(p.getName(), DataTypes.StringType, true, Metadata.empty))
+    val structFields = properties.map(p => convertWeaviateTypesToStructField(p))
     new StructType(structFields.toArray)
   }
+
+  def convertWeaviateTypesToStructField(property: Property): StructField = {
+    val dataType: DataType = convertWeaviateDatatypeToSparkDataType(property.getDataType)
+    StructField(property.getName(), dataType, true, Metadata.empty)
+  }
+
+  def convertWeaviateDatatypeToSparkDataType(weaviateDataType: util.List[String]): DataType = {
+
+  }
+
   override def getTable(schema: StructType, partitioning: Array[Transform], properties: util.Map[String, String]): Table =
     WeaviateCluster()
 }
